5b76a2398580d9ff81dbf68df3fb7349bb16b0c8
Kuzya <alexkuz@gmail.com>
2014-11-02 17:26:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,140 @@
+'use strict';
+
+var raf = require('raf'),
+    EasingFunctions = require('./EasingFunctions'),
+    isNode = typeof global !== "undefined" && {}.toString.call(global) == '[object global]',
+    noop = function() {};
+
+/**
+ * 
+ *
+ */
+
+function now() {
+  if (!isNode && window.performance && window.performance.now) {
+    return window.performance.now();
+  }
+
+  if (Date.now) {
+    return Date.now();
+  }
+
+  return new Date().getTime();
+}
+
+var Animation = function (options) {
+  var frame = null,
+      lastTargetState = null,
+      currentState = null,
+      animationStack = [],
+      onRender = options.onRender || noop,
+      onFinish = options.onFinish || noop,
+      onCancel = options.onCancel || noop,
+      enabledRAF = options.enabledRAF || options.enabledRAF === undefined,
+      fps = options.fps || 60,
+      stepFunc;
+
+  if (isNode) {
+    enabledRAF = false;
+  }
+
+  stepFunc = enabledRAF ? raf : function(func) { setTimeout(func, 1000 / fps); };
+
+  function isAnimating() {
+    return !!lastTargetState;
+  }
+
+  function finish() {
+    if (lastTargetState !== null) {
+      onFinish(lastTargetState);
+
+      lastTargetState = null;
+      currentState = null;
+    }
+  }
+
+  function cancel() {
+    if (lastTargetState !== null) {
+      if (enabledRAF) {
+        raf.cancel(frame);
+        frame = null;
+      }
+      onCancel();
+
+      lastTargetState = null;
+      currentState = null;
+    }
+  }
+
+  function filterOutdatedTargetsFromStack(time) {
+    var filteredStack = [];
+    for (var i = animationStack.length - 1; i >= 0; i--) {
+      if (animationStack[i].end > time) {
+        filteredStack.push(animationStack[i]);
+      }
+    }
+
+    animationStack = filteredStack;
+  }
+
+  function getCurrentState(time) {
+    var target = lastTargetState,
+        animation,
+        remain;
+
+    for (var i = animationStack.length - 1; i >= 0; i--) {
+      animation = animationStack[i];
+      if (animation.end < time) { continue; }
+      remain = (animation.end - time) / animation.duration;
+      target -= (animation.toY - animation.fromY) * animation.easing(remain);
+    }
+
+    return target;
+  }
+
+  function animationStep() {
+    if (lastTargetState === null) { return; }
+
+    var time = now();
+    
+    currentState = getCurrentState(time);
+
+    onRender(currentState);
+
+    if (currentState !== lastTargetState) {
+      frame = stepFunc(animationStep);
+    } else {
+      finish();
+    }
+  }
+
+  function animate(fromState, toState, duration, easing) {
+    var time = now(),
+        animation;
+
+    animation = {
+      duration: duration,
+      end: time + duration,
+      fromState: lastTargetState === null ? fromState : lastTargetState,
+      toState: toState,
+      easing: ((typeof easing === 'string') ? EasingFunctions[easing] : easing) || EasingFunctions.easeInOutQuad
+    };
+
+    filterOutdatedTargetsFromStack(time);
+
+    animationStack.push(animation);
+
+    lastTargetState = toState;
+
+    frame = stepFunc(animationStep);
+  }
+
+  return {
+    animate: animate,
+    isAnimating: isAnimating,
+    cancel: cancel
+  };
+};
+
+
+module.exports = Animation;
---
+++ test/index.js
@@ -0,0 +1,46 @@
+var Animation = require('../index'),
+    expect = require('chai').expect;
+
+describe('#animate', function() {
+  it('returns final state on finish', function(done) {
+    var animation = new Animation({
+      onFinish: function (finalState) {
+        expect(finalState).to.deep.equal({ x: 100, y: 200 });
+        done();
+      }
+    });
+
+    animation.animate({ x: 0, y: 0 }, { x: 100, y: 200 }, 200);
+  });
+
+  it('cancels animation', function(done) {
+    var animation = new Animation({
+      onRender: function (currentState) {
+        expect(currentState).not.to.deep.equal({ x: 100, y: 200 });
+      },
+      onCancel: function () {
+        done();
+      }
+    });
+
+    animation.animate({ x: 0, y: 0 }, { x: 100, y: 200 }, 400);
+
+    setTimeout(function () {
+      animation.cancel();
+    }, 200);
+  });
+
+  it('returns last animation final state on finish', function(done) {
+    var animation = new Animation({
+      onFinish: function (finalState) {
+        expect(finalState).to.deep.equal({ x: 300, y: 400 });
+        done();
+      }
+    });
+
+    animation.animate({ x: 0, y: 0 }, { x: 100, y: 200 }, 400);
+    setTimeout(function () {
+      animation.animate({ x: 100, y: 200 }, { x: 300, y: 400 }, 400);
+    }, 200);
+  });
+});
